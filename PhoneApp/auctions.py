4575480cdc4b2eb5c918e0bba2f5495ad19c3167
DimFury <56711057+DimFury@users.noreply.github.com>
2020-10-16 02:48:29 +1000

    I have added a create.html
    (opening bid will need adjusting) but it works from when i tried it, we have date inputs and image upload function -Ayden

--- PhoneApp/auctions.py
+++ PhoneApp/auctions.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, render_template, url_for, redirect, request
 from .models import Auctions, Review
 from .forms import AuctionsForm, ReviewForm
-from .forms import AuctionsForm, ReviewForm
+
 
 from datetime import datetime
 from . import db
 import os
 from werkzeug.utils import secure_filename
+from flask_wtf import form
+from wtforms.fields.core import DateField
 
 
 # create blueprint
@@ -15,5 +17,38 @@ bp = Blueprint('auction', __name__, url_prefix='/auctions')
 
 @bp.route('/<id>')
 def show(id):
-    #destination = get_destination()
-    return render_template('auctions/show.html')
+    auction = Auctions.query.filter_by(id=id).first()
+    comment_form = ReviewForm()
+    return render_template('auctions/show.html', auctions=auction, form=comment_form)
+
+@bp.route('/<id>/comment', methods=['GET', 'POST'])
+def comment(id):
+    comment_form = ReviewForm()
+    if form.validate_on_submit():
+        print("Comment posted by the user:  ", comment_form.text.data)
+    return redirect(url_for('auction.show', id=1))
+
+@bp.route('/create', methods=['GET', 'POST'])
+def create():
+    print('Method type: ', request.method)
+    create_form = AuctionsForm()
+    if create_form.validate_on_submit():
+        auctions = Auctions(name=create_form.name.data,
+                            brand=create_form.brand.data,
+                            model=create_form.model.data,
+                            condition=create_form.model.data,
+                            description=create_form.model.data,
+                            image=create_form.image.data,
+                            open_bid=create_form.open_bid.data,
+                            start=create_form.open_bid.data)
+
+
+        db.session.add(auctions)
+        db.session.commit()
+
+    
+        print('Successfully created new auction listing', 'success')
+        return redirect(url_for('auction.create'))
+
+    return render_template('auctions/create.html', form=create_form)
+
--- PhoneApp/forms.py
+++ PhoneApp/forms.py
@@ -2,24 +2,33 @@ from flask_wtf import FlaskForm
 from wtforms.fields import TextAreaField, SubmitField, StringField, PasswordField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 from flask_wtf.file import FileRequired, FileField, FileAllowed
+from wtforms import Form, BooleanField, StringField, validators, DateTimeField, IntegerField
+from wtforms.fields.html5 import DateField, IntegerField
 
 ALLOWED_FILE = ['png', 'jpg', 'JPG', 'PNG']
 
 
 class AuctionsForm(FlaskForm):
-    name = StringField('Country', validators=[
-                       InputRequired('Country is required')])
+    #name = StringField('', validators=[
+    #                   InputRequired('Country is required')])
+    brand = StringField('Mobile Brand', validators=[
+                        InputRequired('Mobile Brand Is Required')])
+    
     image = StringField('Image', validators=[
-                        InputRequired('Image is required')])
-    # brand
-    # model
-    # condition
+                        InputRequired('Image is Required')])
+    model = StringField('Model No.', validators=[
+                        InputRequired('Model No. Is Required')])
+    condition = StringField('Condition Of The Product', validators=[
+                            InputRequired('Condition of the product is Required')])
     description = StringField('Description', validators=[InputRequired('Description is required'),
                                                          Length(min=10, max=300, message='Description is too short or too long')])
-    image = FileField('Destination Image', validators=[
+    image = FileField('Mobile Images', validators=[
         FileRequired(message='Image can not be empty'),
         FileAllowed(ALLOWED_FILE, message="Only supports valid filetypes")])
 
+    open_bid = IntegerField('Opening Bid', [validators.NumberRange(min=0, max=10)])
+    start = DateField('Start Date', id='datepick')
+    end = DateField('End Date', id='datepick')    
     submit = SubmitField('Create')
 
 
