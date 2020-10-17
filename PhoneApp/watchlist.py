8c13b8c4e1176adefad1e37e1430ec68467dadb2
sirahut <n9923471@qut.edu.au>
2020-10-17 21:41:53 +1000

    remove useless comment

--- PhoneApp/watchlist.py
+++ PhoneApp/watchlist.py
@@ -40,22 +40,3 @@ def add_to_watchlist(id):
     #         print('Review form is invalid')
     # # notice the signature of url_for
     return redirect(url_for('auction.show', id=id))
-# @bp.route('/<id>/addWatchlist', methods=['GET', 'POST'])
-# @login_required
-# def add_to_watchlist(id):
-
-#         # here the form is created
-#         review_form_instance = ReviewForm()
-#         auction_obj = Auctions.query.filter_by(id=id).first()
-#         review = Review(text=review_form_instance.review.data,
-#                         auction=auction_obj, user=current_user)
-
-#         db.session.add(review)
-#         db.session.commit()
-#         if review_form_instance.validate_on_submit():  # this is true only in case of POST method
-#             print(
-#                 f'Review form is valid. The review was {review_form_instance.review.data}')
-#         else:
-#             print('Review form is invalid')
-#     # notice the signature of url_for
-#         return redirect(url_for('auction.show', id=id)
