4cc3918858b32e96c17c684282e6037110d2eb33
DimFury <56711057+DimFury@users.noreply.github.com>
2020-10-25 13:07:17 +1000

    delete function to delete listed items added
    delete function added
    edit function broke currently fixing
    new html
    listings now show only users listed items and now also presents security wall to other users lists.

--- PhoneApp/auctions.py
+++ PhoneApp/auctions.py
@@ -186,27 +186,32 @@ def listed(id):
     auction = Auctions.query.filter_by(user_id=id).all()
     
     if user != current_user:
-        flash(url_for('You Cannot Access this Users Listed Items'))
-        return redirect(url_for('auction.listed', id=id))
+        return redirect(url_for('main.index', id=id))
     return render_template('auctions/listed.html', user=user)
 
 
 
-@bp.route("/auctions/listed/<id>/delete", methods=['POST'])
+@bp.route("/delete/<id>", methods=['GET', 'POST'])
 @login_required
 def delete_component(id):
-    engine = create_engine('sqlite:///techdrop.sqlite')
-    cur = engine.connection()
+    user = User.query.filter_by(id=id).first()
+    if request.method == 'POST':
+        deleted = Auctions.query.filter_by(id=id).delete()
+        db.session.commit()
+        return render_template('auctions/listed.html', id=id, user=user)
+    else:
+        return("/")
 
-    cur.execute("DELETE FROM auctions WHERE id =%s", [id])
+@bp.route("/listed/<id>/updated", methods=['GET', 'POST'])
+@login_required
+def edit_component(id):
+   
+    user = Auctions.query.filter_by(id=id)
+    auction_item = AuctionsForm()
+#currently empty function
+    
+    return render_template('auctions/update.html', id=id, user=user, form=auction_item)
 
-    engine.connection.commit()
-
-    cur.close()
-
-    flash('Article Deleted', 'Success')
-
-    return render_template('auctions/index.html', user=user)
 
 
 
