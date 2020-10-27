d54daa88c8413c801d4a353d74dafdc001438145
DimFury <56711057+DimFury@users.noreply.github.com>
2020-10-27 12:12:04 +1000

    adjustments

--- PhoneApp/views.py
+++ PhoneApp/views.py
@@ -1,12 +1,23 @@
+<<<<<<< Updated upstream
 from flask import Blueprint, render_template, request, session, redirect, url_for
 from .models import Model
+=======
+from flask import Blueprint, render_template, request, session
+from PhoneApp.forms import AuctionsForm
+from PhoneApp.models import Auctions, User
+>>>>>>> Stashed changes
 
 bp = Blueprint('main', __name__)
 
 
 @bp.route('/')
 def index():
-    return render_template('index.html')
+    auction_item = AuctionsForm()
+    user = User.query.filter_by().first()
+    auction = Auctions.query.filter_by().all()
+
+
+    return render_template('index.html', user=user)
 
 # def search():
 
